a3cd09fabb643a6dc30ade591a4a96b94bbde886
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2023-02-02 14:43:37 +0700

    Test 1

--- src/AkmalFairuz/McMMO/command/McmmoSetupCommand.php
+++ src/AkmalFairuz/McMMO/command/McmmoSetupCommand.php
@@ -22,14 +22,12 @@ use pocketmine\command\Command;
 use pocketmine\entity\Entity;
 use pocketmine\player\Player;
 
-use pocketmine\plugin\PluginOwned;
-
 use AkmalFairuz\McMMO\entity\FloatingText;
 use AkmalFairuz\McMMO\Main;
 
-class McmmoSetupCommand extends Command implements PluginOwned {
+class McmmoSetupCommand extends Command {
 
-    public function __construct(private Main $owner) {
+    public function __construct() {
         parent::__construct("mcmmoadmin", "Admin McMMO Command", null, []);
         $this->setPermission("mcmmo.admin");
     }
@@ -39,10 +37,12 @@ class McmmoSetupCommand extends Command implements PluginOwned {
             $sender->sendMessage("Please use command in-game");
             return true;
         }
+
         if(!$sender->hasPermission("mcmmo.admin")) {
             $sender->sendMessage("You don't have permission to use this command");
             return true;
         }
+
         $a = ["lumberjack", "farmer", "excavation", "miner", "killer", "combat", "builder", "consumer", "archer", "lawnmower"];
         if(count($args) < 1) {
             $sender->sendMessage("Usage: /mcmmoadmin setup ".implode("/" , $a)."> (to spawn floating text) | /mcmmoadmin remove (to remove nearly floating text)");
@@ -70,12 +70,17 @@ class McmmoSetupCommand extends Command implements PluginOwned {
                 return true;
             }
             $a = ["lumberjack" => 0, "farmer" => 1, "excavation" => 2, "miner" => 3, "killer" => 4, "combat" => 5, "builder" => 6, "consumer" => 7, "archer" => 8, "lawnmower" => 9];
-            $this->getOwningPlugin()->spawnFloatingText($a[$args[1]], $sender);
+            $nbt = CompoundTag::create();
+            $nbt->setTag("Name", new StringTag($sender->getSkin()->getSkinId()));
+            $nbt->setTag("Data", new ByteArrayTag($sender->getSkin()->getSkinData()));
+            $nbt->setTag("CapeData", new ByteArrayTag($sender->getSkin()->getCapeData()));
+            $nbt->setTag("GeometryName", new StringTag($sender->getSkin()->getGeometryName()));
+            $nbt->setTag("GeometryData", new ByteArrayTag($sender->getSkin()->getGeometryData()));
+            $nbt->setInt("type", $a[$args[1]]);
+            $entity = new FloatingText(Location::fromObject($sender->getPosition(), $sender->getPosition()->getWorld(), $sender->getLocation()->getYaw(), $sender->getLocation()->getPitch()), $sender->getSkin(), $nbt);
+            $entity->setNameTagAlwaysVisible(true);
+            $entity->spawnToAll();
         }
         return true;
     }
-
-    public function getOwningPlugin() : Main {
-		return $this->owner;
-	}
 }
--- src/AkmalFairuz/McMMO/entity/FloatingText.php
+++ src/AkmalFairuz/McMMO/entity/FloatingText.php
@@ -17,41 +17,65 @@
 namespace AkmalFairuz\McMMO\entity;
 
 use pocketmine\entity\Human;
-use pocketmine\entity\Ageable;
-use pocketmine\entity\EntitySizeInfo;
 
 use pocketmine\player\Player;
 
 use pocketmine\nbt\tag\CompoundTag;
 
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\network\mcpe\protocol\SetActorDataPacket;
 use pocketmine\utils\TextFormat;
 
 use AkmalFairuz\McMMO\Main;
 
-class FloatingText extends Human implements Ageable {
+class FloatingText extends Human {
 
-    private $baby = false;
+    public $updateTick = 0;
 
-    protected $gravity = 0;
-
-    protected function getInitialSizeInfo() : EntitySizeInfo {
-		return new EntitySizeInfo(0, 0);
-	}
+    public $type = 0;
 
     public function getName() : string {
 		return "FloatingText";
 	}
 
-    public function isBaby() : bool {
-		return $this->baby;
-    }
-
 	public function initEntity(CompoundTag $nbt) : void {
 		parent::initEntity($nbt);
 		$this->setNameTagAlwaysVisible(true);
 		$this->setScale(0.0000000000000000000000000000000001);
+		$this->updateTick = 0;
+        $this->type = $this->namedtag->getInt("type");
 	}
+
+	public function onUpdate(int $currentTick) {
+        parent::onUpdate($currentTick);
+        $this->setImmobile(true);
+		$this->updateTick++;
+        if($this->updateTick == 20) {
+            $this->updateTick = 0;
+            $a = ["Lumberjack", "Farmer", "Excavation", "Miner", "Killer", "Combat", "Builder", "Consumer", "Archer", "Lawn Mower"];
+            $l = "";
+            $i = 0;
+            $lead = Main::getInstance()->getAll($this->type);
+            arsort($lead);
+            foreach($lead as $k => $o) {
+                if($i == 20) break;
+                $i++;
+                $l .= TextFormat::RED. $i . ") " . TextFormat::GREEN . $k . TextFormat::RED . " : " . TextFormat::BLUE . "Lv. " . $o . "\n";
+            }
+            $this->setNameTag(TextFormat::BOLD . TextFormat::AQUA . "MCMMO Leaderboard\n" . TextFormat::RESET . TextFormat::YELLOW . $a[$this->type] . TextFormat::RESET . "\n\n".$l);
+            foreach ($this->getViewers() as $player) {
+                $this->sendNameTag($player);
+            }
+        }
+        return true; 
+    }
+
+	public function sendNameTag(Player $player): void {
+        $pk = new SetActorDataPacket();
+        $pk->entityRuntimeId = $this->getId();
+        $pk->metadata = [self::DATA_NAMETAG => [self::DATA_TYPE_STRING, $this->getNameTag()]];
+        $player->dataPacket($pk);
+    }
 
 	public function attack(EntityDamageEvent $source) : void {
 		$source->cancel();
