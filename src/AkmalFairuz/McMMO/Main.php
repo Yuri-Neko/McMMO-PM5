4fee07457b70a60ea12982a377dfdd63066ee435
AraaCuteUwU <raihanxstorm14@gmail.com>
2023-10-16 17:01:12 +0800

    Gagal commit

--- src/AkmalFairuz/McMMO/Main.php
+++ src/AkmalFairuz/McMMO/Main.php
@@ -48,7 +48,8 @@ use AkmalFairuz\McMMO\command\McmmoCommand;
 use AkmalFairuz\McMMO\command\McmmoSetupCommand;
 use AkmalFairuz\McMMO\entity\FloatingText;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
     use SingletonTrait;
 
     public const LUMBERJACK = 0;
@@ -68,14 +69,15 @@ class Main extends PluginBase implements Listener {
 
     public ?EconomyLand $economyLand;
 
-    public function onEnable() : void {
+    public function onEnable(): void
+    {
         $this->saveDefaultConfig();
         $this->saveResource("database.yml");
         $this->getServer()->getCommandMap()->register("mcmmo", new McmmoCommand());
         $this->getServer()->getCommandMap()->register("mcmmoadmin", new McmmoSetupCommand());
         $this->database = yaml_parse(file_get_contents($this->getDataFolder() . "database.yml"));
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        EntityFactory::getInstance()->register(FloatingText::class, function (World $world, CompoundTag $nbt) : FloatingText {
+        EntityFactory::getInstance()->register(FloatingText::class, function (World $world, CompoundTag $nbt): FloatingText {
             return new FloatingText(EntityDataHelper::parseLocation($nbt, $world), Human::parseSkinNBT($nbt), $nbt);
         }, ["FloatingText", "FloatingTextEntity"]);
         self::setInstance($this);
@@ -83,22 +85,26 @@ class Main extends PluginBase implements Listener {
         $this->economyLand = $this->getServer()->getPluginManager()->getPlugin('EconomyLand');
     }
 
-    public function onDisable() : void {
+    public function onDisable(): void
+    {
         file_put_contents($this->getDataFolder() . "database.yml", yaml_emit($this->database));
         // sleep(3); // save database delay
     }
 
-    public function getXp(int $type, Player $player) : int {
+    public function getXp(int $type, Player $player): int
+    {
         return $this->database["xp"][$type][strtolower($player->getName())];
     }
 
-    public function getLevel(int $type, Player $player) : int {
+    public function getLevel(int $type, Player $player): int
+    {
         return $this->database["level"][$type][strtolower($player->getName())];
     }
 
-    public function addXp(int $type, Player $player) {
+    public function addXp(int $type, Player $player)
+    {
         $this->database["xp"][$type][strtolower($player->getName())]++;
-        if($this->database["xp"][$type][strtolower($player->getName())] >= ($this->getLevel($type, $player) * 100)) {
+        if ($this->database["xp"][$type][strtolower($player->getName())] >= ($this->getLevel($type, $player) * 100)) {
             $this->database["xp"][$type][strtolower($player->getName())] = 0;
             $this->addLevel($type, $player);
         }
@@ -106,20 +112,23 @@ class Main extends PluginBase implements Listener {
         $player->sendTip(str_replace(["{type}", "{xp}"], [$a[$type], $this->getXp($type, $player)], $this->getConfig()->get("message")["progress"]));
     }
 
-    public function addLevel(int $type, Player $player) {
+    public function addLevel(int $type, Player $player)
+    {
         $this->database["level"][$type][strtolower($player->getName())]++;
         $a = ["Lumberjack", "Farmer", "Excavation", "Miner", "Killer", "Combat", "Builder", "Consumer", "Archer", "Lawn Mower"];
         $player->sendTip(str_replace(["{type}", "{level}"], [$a[$type], $this->getLevel($type, $player)], $this->getConfig()->get("message")["progress"]));
     }
 
-    public function getAll(int $type) : array {
+    public function getAll(int $type): array
+    {
         return $this->database["level"][$type];
     }
 
-    public function onLogin(PlayerLoginEvent $event) {
+    public function onLogin(PlayerLoginEvent $event)
+    {
         $player = $event->getPlayer();
-        if(!isset($this->database["xp"][0][strtolower($player->getName())])) {
-            for($i = 0; $i < 10; $i++) {
+        if (!isset($this->database["xp"][0][strtolower($player->getName())])) {
+            for ($i = 0; $i < 10; $i++) {
                 $this->database["xp"][$i][strtolower($player->getName())] = 0;
                 $this->database["level"][$i][strtolower($player->getName())] = 1;
             }
@@ -129,18 +138,19 @@ class Main extends PluginBase implements Listener {
     /**
      * @priority LOWEST
      */
-    public function onBreak(BlockBreakEvent $event) {
-        if($event->isCancelled()) {
+    public function onBreak(BlockBreakEvent $event)
+    {
+        if ($event->isCancelled()) {
             return;
         }
 
         // if (!$this->economyLand->permissionCheck($event)) {
         // 	return;
         // }
-        
+
         $player = $event->getPlayer();
         $block = $event->getBlock();
-        switch($block->getTypeId()) {
+        switch ($block->getTypeId()) {
             case BlockTypeIds::WHEAT:
             case BlockTypeIds::BEETROOTS:
             case BlockTypeIds::PUMPKIN_STEM:
@@ -201,18 +211,19 @@ class Main extends PluginBase implements Listener {
     /**
      * @priority LOWEST
      */
-    public function onPlace(BlockPlaceEvent $event) {
-        if($event->isCancelled()) {
+    public function onPlace(BlockPlaceEvent $event)
+    {
+        if ($event->isCancelled()) {
             return;
         }
-        
+
         // if (!$this->economyLand->permissionCheck($event)) {
         // 	return;
         // }
-        
+
         $player = $event->getPlayer();
         $block = $event->getBlockAgainst();
-        if($block instanceof Opaque) {
+        if ($block instanceof Opaque) {
             $this->addXp(self::BUILDER, $player);
         }
     }
@@ -220,14 +231,15 @@ class Main extends PluginBase implements Listener {
     /**
      * @priority LOWEST
      */
-    public function onDamage(EntityDamageEvent $event) {
+    public function onDamage(EntityDamageEvent $event)
+    {
         $entity = $event->getEntity();
-        if($event->isCancelled()) {
+        if ($event->isCancelled()) {
             return;
         }
-        if($event instanceof EntityDamageByEntityEvent) {
-            if(!$entity instanceof Player) return;
-            if(($damager = $event->getDamager()) instanceof Player) {
+        if ($event instanceof EntityDamageByEntityEvent) {
+            if (!$entity instanceof Player) return;
+            if (($damager = $event->getDamager()) instanceof Player) {
                 if (($entity->getHealth() - $event->getFinalDamage()) <= 0) {
                     $this->addXp(self::KILLER, $damager);
                 }
@@ -239,12 +251,13 @@ class Main extends PluginBase implements Listener {
     /**
      * @priority LOWEST
      */
-    public function onShootBow(EntityShootBowEvent $event) {
-        if($event->isCancelled()) {
+    public function onShootBow(EntityShootBowEvent $event)
+    {
+        if ($event->isCancelled()) {
             return;
         }
         $entity = $event->getEntity();
-        if($entity instanceof Player) {
+        if ($entity instanceof Player) {
             $this->addXp(self::ARCHER, $entity);
         }
     }
@@ -252,8 +265,9 @@ class Main extends PluginBase implements Listener {
     /**
      * @priority LOWEST
      */
-    public function onItemConsume(PlayerItemConsumeEvent $event) {
-        if($event->getPlayer()->getHungerManager()->getFood() < $event->getPlayer()->getHungerManager()->getMaxFood()) {
+    public function onItemConsume(PlayerItemConsumeEvent $event)
+    {
+        if ($event->getPlayer()->getHungerManager()->getFood() < $event->getPlayer()->getHungerManager()->getMaxFood()) {
             $this->addXp(self::CONSUMER, $event->getPlayer());
         }
     }
